6fde35c7e6ed6fc57f17570e40960798f11d7c07
Kevin Stewart <kstewart@zynga.com>
2020-09-05 15:07:15 -0400

    Add ability to do opposed dice rolls

--- dice.py
+++ dice.py
@@ -22,6 +22,20 @@ def run_simulations(num, simulations, kata, rerolls):
 	plot.show()
 
 
+def run_opposed(attack, defence, simulations, kata, rerolls, defender_kata, defender_rerolls):
+	results = []
+
+	for i in range(simulations):
+		results.append(run_simulation(attack, kata, rerolls) - run_simulation(defence, defender_kata, defender_rerolls))
+
+	hist = Counter(results)
+	
+	keys = hist.keys()
+	vals = hist.values()
+
+	plot.bar(keys, np.divide(list(vals), simulations))
+	plot.show()
+
 def run_simulation(num, kata, rerolls):
 	rolls = []
 	for i in range(num):
@@ -50,7 +64,13 @@ if __name__ == '__main__':
     parser.add_argument('-s', '--simulations', required=True, help="the numnber of simulations to run", type=int)
     parser.add_argument('-k', '--kata', default=False, help="does the model have Kata")
     parser.add_argument('-rr', '--rerolls', default=0, help='the number of rerolls', type=int)
+    parser.add_argument('-d', '--defence', default=0, help='the number of dice to defend with, if included will run opposed simulations', type=int)
+    parser.add_argument('-dk', '--d_kata', default=False, help="does the defender have Kata")
+    parser.add_argument('-drr', '--d_rerolls', default=0, help='the number of rerolls for the defender', type=int)
 
     args = parser.parse_args()
 
-    run_simulations(args.num, args.simulations, args.kata, args.rerolls)
+    if args.defence == 0:
+    	run_simulations(args.num, args.simulations, args.kata, args.rerolls)
+    else:
+    	run_opposed(args.num, args.defence, args.simulations, args.kata, args.rerolls, args.d_kata, args.d_rerolls)
